ee911405851cfbfab203c5b6aefdee15d7b640fb
Christophe Asselin <christophe.asselin@hotmail.com>
2019-08-17 16:45:18 -0400

    Added AppBar to App Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import AppBar from './Components/AppBar';
+import TopAppBarFixedAdjust from '@material/react-top-app-bar';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+            <AppBar></AppBar>
+            <TopAppBarFixedAdjust></TopAppBarFixedAdjust>
+        </div>
+    );
 }
 
 export default App;
