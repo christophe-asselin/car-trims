37c0c013304c9782ccdfc34a2bbca60b1f110c54
Christophe Asselin <christophe.asselin@hotmail.com>
2019-08-19 22:57:07 -0400

    Updates

---
+++ precache-manifest.b52ba73acf2ba1ef1f47d28e92e644fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe52888ff7a5a1cc64ee07a407110093",
+    "url": "/christophe-asselin/car-trims/index.html"
+  },
+  {
+    "revision": "40baf1e14ea58f27217b",
+    "url": "/christophe-asselin/car-trims/static/css/2.7af277c2.chunk.css"
+  },
+  {
+    "revision": "6d416c30e339034f03a2",
+    "url": "/christophe-asselin/car-trims/static/css/main.25ab1c48.chunk.css"
+  },
+  {
+    "revision": "40baf1e14ea58f27217b",
+    "url": "/christophe-asselin/car-trims/static/js/2.083cf760.chunk.js"
+  },
+  {
+    "revision": "6d416c30e339034f03a2",
+    "url": "/christophe-asselin/car-trims/static/js/main.29ca7d95.chunk.js"
+  },
+  {
+    "revision": "a60bf173a5bb73ba3c27",
+    "url": "/christophe-asselin/car-trims/static/js/runtime~main.e5ce7b84.js"
+  },
+  {
+    "revision": "0a1d8fb09dfa6a086b63b997faefe64b",
+    "url": "/christophe-asselin/car-trims/static/media/car_background.0a1d8fb0.jpg"
+  }
+]);
